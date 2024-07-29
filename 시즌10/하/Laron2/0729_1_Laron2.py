002401b5138dc1ee1f4d12fd6d7711b28c72929a
구명준 <146055640+Laron2@users.noreply.github.com>
2024-07-30 03:06:30 +0900

    Update 0729_1_Laron2.py

--- 시즌10/하/Laron2/0729_1_Laron2.py
+++ 시즌10/하/Laron2/0729_1_Laron2.py
@@ -3,9 +3,9 @@ key = n
 cnt = 0
 
 while True:
-    cnt+=1
-    sum = n//10 + n%10
-    n = n%10*10 + sum%10
+    cnt += 1
+    sum = n // 10 + n % 10
+    n = n % 10 * 10 + sum % 10
     if (n == key):
         break
 print(cnt)
